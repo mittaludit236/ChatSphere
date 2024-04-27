ee1eb0dc26c2c813f1e1963cfd1b460e9d06e53d
Udit Mittal <uditmittal@Udits-MacBook-Air.local>
2024-04-27 08:41:16 +0530

    srch_comments_backend

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,4 +1,17 @@
 const mongoose=require("mongoose");
+const CommentSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        max: 500
+    },
+    // No need to separately add date and time fields; timestamps will handle it
+}, {
+    timestamps: true // This will automatically add `createdAt` and `updatedAt` fields
+});
 const PostSchema=new mongoose.Schema({
     userId:{
         type: String,
@@ -15,6 +28,7 @@ const PostSchema=new mongoose.Schema({
         type: Array,
         default:[]
     },
+    comments: [CommentSchema],
 },
 {timestamps: true}
 );
--- api/routes/messages.js
+++ api/routes/messages.js
@@ -1,5 +1,6 @@
 const router=require("express").Router();
 const Message=require("../models/Message");
+const User=require("../models/User");
 router.post("/",async(req,res)=>{
     const newm=new Message(req.body);
     try{
@@ -20,5 +21,38 @@ router.get("/:conversationId",async(req,res)=>{
     {
         res.status(500).json(err);
     }
-})
+});
+router.post("/search", async (req, res) => {
+    const find_prof = req.body.find_prof;
+    const userId = req.body.userId; // assuming you receive a userId from the frontend
+
+    try {
+        if (find_prof === '') {
+            res.status(200).json([]);
+        } else {
+            // Fetch the user and their followings
+            const mainUser = await User.findById(userId);
+            if (!mainUser) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            const followings = mainUser.followings; // assuming 'followings' is an array of userIds
+
+            // Fetch the details of users being followed
+            const followingUsers = await User.find({
+                '_id': { $in: followings }
+            });
+
+            // Filter these users based on the regex pattern provided
+            const filteredUsers = followingUsers.filter(user =>
+                new RegExp(find_prof, 'i').test(user.username)
+            );
+
+            res.status(200).json(filteredUsers);
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports=router;
