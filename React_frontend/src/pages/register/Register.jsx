b07d7970d81aba771add38e346a26514005ce586
aryanmaurya45 <aryan.maurya2104@gmail.com>
2024-06-01 12:23:12 +0530

    message

--- React_frontend/src/pages/register/Register.jsx
+++ React_frontend/src/pages/register/Register.jsx
@@ -33,8 +33,8 @@ export default function Register() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-3/4 h-3/4 flex">
         <div className="flex-1 flex flex-col justify-center px-10">
-          <h3 className="text-5xl font-bold text-blue-600">Lamasocial</h3>
-          <p className="text-2xl text-gray-500 mt-2">Connect with friends and the world around you on Lamasocial.</p>
+          <h3 className="text-5xl font-bold text-blue-600">ChatSphere</h3>
+          <p className="text-2xl text-gray-500 mt-2">Connect with friends and the world around you on ChatSphere</p>
         </div>
         <div className="flex-1 flex flex-col justify-center px-10">
           <div className="h-96 p-8 bg-white rounded-lg flex flex-col justify-between">
